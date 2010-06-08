8147828b476c31c09a34582f7c6c18f30694ea52
Damien Tournoud <damien@commerceguys.com>
2010-06-09 01:49:12 +0200

    Force an hostname for cron runs, or PIFR will not be able to launch itself properly.

--- drupaltestbot/debian/etc/cron.d
+++ drupaltestbot/debian/etc/cron.d
@@ -1,1 +1,1 @@
-*/1 * * * *     www-data   [ -x /usr/bin/drush ] && /usr/bin/drush -r /var/lib/drupaltestbot core-cron
+*/1 * * * *     www-data   [ -x /usr/bin/drush ] && /usr/bin/drush -l `/bin/hostname` -r /var/lib/drupaltestbot core-cron
